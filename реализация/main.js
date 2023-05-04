ff656e5e259ca3195d2ac2ba00ab5f535c06f730
Marta <118897672+martineidger@users.noreply.github.com>
2023-05-04 22:21:52 +0300

    Delete реализация directory

--- реализация/main.js
+++
@@ -1,7 +0,0 @@
-
-const but_more = document.querySelector('.but-more');
-const ev = document.querySelector('.ev');
-but_more.addEventListener('click', () => {
-    ev.classList.toggle('ev-hidden');
-});
-
--- реализация/script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector('.btn');
-btn.addEventListener('click', reset);
-function reset()
-{
-    document.getElementById("myForm-n").value = "";
-    document.getElementById("myForm-t").value = "";
-    document.getElementById("myForm-e").value = "";
-    document.getElementById("myForm-m").value = "";
-}
